34f0ce479b9a3107fabf1addb10133045287ecc9
Edward Yu <3dward.yu@gmail.com>
2017-05-13 01:33:39 -0400

    add local login

--- config/passport.js
+++ config/passport.js
@@ -7,55 +7,81 @@ var User = require('../models/user');
 
 module.exports = function(passport){
 
-//used to serialize the user for the session
-passport.serializeUser(function(user, done){
-    done(null, user.id);
-});
-
-//used to deserialize the user
-passport.deserializeUser(function(id, done){
-    User.findById(id, function(err, user){
-        done(err, user);
+    //used to serialize the user for the session
+    passport.serializeUser(function(user, done){
+        done(null, user.id);
     });
-});
 
-//Local signup
-passport.use('local-signup', new LocalStrategy({
-    // overwrite username field to use email
-    usernameField: 'email',
-    passwordField: 'password',
-    passReqToCallback: true //allows entire request to be passed back to the callback
-},
-function(req, email, password, done){
+    //used to deserialize the user
+    passport.deserializeUser(function(id, done){
+        User.findById(id, function(err, user){
+            done(err, user);
+        });
+    });
 
-    process.nextTick(function(){
+    //Local signup
+    passport.use('local-signup', new LocalStrategy({
+        // overwrite username field to use email
+        usernameField: 'email',
+        passwordField: 'password',
+        passReqToCallback: true //allows entire request to be passed back to the callback
+    },
 
-        // check if user is trying to login exists
+    function(req, email, password, done){
+
+        process.nextTick(function(){
+
+            // check if user is trying to login exists
+            User.findOne({ 'local.email': email }, function(err, user){
+
+                if (err)
+                    return done(err);
+
+                // check to see if there's already a user with that email
+                if (user) {
+                    return done(null, false, req.flash('signupMessage', 'That email is already taken.'))
+                } else {
+
+                    //if there is no user, create one
+                    var newUser = new User();
+
+                    // set user's credentials
+                    newUser.local.email = email;
+                    newUser.local.password = newUser.encryptPassword(password);
+
+                    // save the user
+                    newUser.save(function(err){
+                        if (err)
+                            throw err;
+                        return done(null, newUser);
+                    });
+                }
+            });
+        });
+    }));
+
+    //Local login
+    passport.use('local-login', new LocalStrategy({
+        // overwrite username field to use email
+        usernameField: 'email',
+        passwordField: 'password',
+        passReqToCallback: true //allows entire request to be passed back to the callback
+    },
+
+    function(req, email, password, done){
+
+        // finds a user's email that's the same as form
         User.findOne({ 'local.email': email }, function(err, user){
 
             if (err)
                 return done(err);
 
-            // check to see if there's already a user with that email
-            if (user) {
-                return done(null, false, req.flash('signupMessage', 'That email is already taken.'))
-            } else {
-
-                //if there is no user, create one
-                var newUser = new User();
-
-                // set user's credentials
-                newUser.local.email = email;
-                newUser.local.password = newUser.encryptPassword(password);
-
-                // save the user
-                newUser.save(function(err){
-                    if (err)
-                        throw err;
-                    return done(null, newUser);
-                });
-            }
+            // return message if user doesn't exist or if password is incorrect
+            if (!user || !user.validPassword(password)) 
+                return done(null, false, req.flash('loginMessage', 'User and/or password combination incorrect.'))
+            
+            //return sucessful user
+            return done(null, user);
         });
-    });
-}));
+    }));
 };
